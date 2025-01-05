cd3fa0611a96b00673cf1a9d0c694e666ead713d
Sagarsaini007 <lokendrasaini2005@gmail.com>
2025-01-05 17:22:12 +0530

    info.py

--- info.py
+++ info.py
@@ -1,10 +1,10 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -100
-ADMIN        = 
+API_ID       = 27901232
+API_HASH     = "c3570337b124e4c1d43c9b1ff557ee67"
+BOT_TOKEN    = "7603807412:AAGQteWXYyMq2JavEfh9Xl0kHyrshZrWnT0"
+SESSION      = "BQGpvTAAqq3gz1CuvRcCIefi950jx277theZII9C_aLC9unQw15TnCUpnULjyKZ5bQe4UyZhOMx6crfH3dEFClIfg76tcKCrNgdra8q2fO14JdXXyaVbIlE6amuQb1aNQmc1Jt5WUrolQl9SMOlZcaW18Ci7myESMzPXEmgkHtZnRZWas1gFFJ_sIA2WboqY-T10-Th51_Jxue-_eNjJGV_y_GRmoJJN8DidCCviIfbLLdc2nw3mHWZPyRQmV2lJ49E0zegfjCfC3QiPOFdhgQeDmgwK2pthw5AbWcSRlFi00_2eA3CD2144MRzWfqVc9qfSiYGzuidwXl0bGD68zTpdT8M94wAAAAB2COJ7AA"
+DATABASE_URI = "mongodb+srv://lokendrasaini9galaxy:f0TVDwu5pVrHn5i6@cluster0.zseht.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+LOG_CHANNEL  = -1002479013444
+ADMIN        = 1980293755
 
 
 # Don't Remove Credit Tg - https://t.me/Prime_Botz
