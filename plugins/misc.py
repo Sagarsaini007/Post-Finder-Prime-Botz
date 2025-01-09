7f280f479b55968baaf3a913d5af13d2981ef688
Amarsingh008 <amarsinghrj92@gmail.com>
2025-01-09 20:01:56 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -7,9 +7,6 @@ from utils import *
 from pyrogram import Client, filters 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
 
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
 @Client.on_message(filters.command("start") & ~filters.channel)
 async def start(bot, message):
     await add_user(message.from_user.id, message.from_user.first_name)
@@ -22,7 +19,6 @@ async def start(bot, message):
         chat_id=message.chat.id,
         photo=image_path,
         caption=script.START.format(message.from_user.mention),
-        disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup([
             [
                 InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕', url='https://t.me/Contra69_bot?startgroup=true')
